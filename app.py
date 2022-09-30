23caf7293b1184e4014d323eb849162f09782a01
Maykon <maykon.marcos.junior@grad.ufsc.br>
2022-09-30 10:58:52 -0300

    app alterado

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+dicioYoda = {'Bom Dia': 'Bom dia pra quem?', 
+             'Qual o seu nome?': 'É sério que você quer que eu repita? É Yoda!!!',
+             'Quero um conselho': 'Não tenho filho desse tamanho.',
+             'Adeus': 'Já vai tarde!'}
+lista_bots = [BotZangado("Yoda", dicioYoda)]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
